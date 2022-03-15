855705bf7e4f20ec73df6f7f15ed51535e513a97
Maxi Ceballos <maxi@MBP-de-Maxi.fibertel.com.ar>
2022-03-15 16:27:23 -0300

    add logic to distroy activity register

--- app/src/main/java/laboratorio/programacion/login/RegisterActivity.java
+++ app/src/main/java/laboratorio/programacion/login/RegisterActivity.java
@@ -2,21 +2,38 @@ package laboratorio.programacion.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
 import laboratorio.programacion.snake.R;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    EditText usuario, password;
+    Button btnRegistrar, btnCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        DAL db = new DAL(this);
+        usuario = (EditText) findViewById(R.id.userRegister);
+        password = (EditText) findViewById(R.id.passRegister);
+
+        btnRegistrar = (Button) findViewById(R.id.btnCreateAccount);
+        btnCancelar = (Button) findViewById(R.id.btnCancel);
+
+        btnRegistrar.setOnClickListener(this);
+        btnCancelar.setOnClickListener(this);
+
+//        DAL db = new DAL(this);
 
         // Insert accounts
 //        Log.d("Insert: ", "Inserting...");
@@ -25,8 +42,8 @@ public class RegisterActivity extends AppCompatActivity {
 //        db.addAccount(new Account("bob789", "123456", 80));
 
         // Read accounts
-        Log.d("Reading: ", "Reading all accounts...");
-        List<Account> accountList = db.getAllAccounts();
+//        Log.d("Reading: ", "Reading all accounts...");
+//        List<Account> accountList = db.getAllAccounts();
 
         // Get 1 account
 //        Account aAccount = db.getAccount("bob456");
@@ -61,5 +78,27 @@ public class RegisterActivity extends AppCompatActivity {
 //        }
 
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent;
+
+        switch (view.getId()) {
+            case R.id.btnCreateAccount:
+//                Account account = new Account(usuario.getText().toString(), password.getText().toString(), 0);
+//                if ( account.isNull()){}
+//                if( dalAccount.insertAccount(account) ) {
+//                    Toast.makeText(this, "CUENTA REGISTRADA", Toast.LENGTH_LONG).show();
+//                } else {
+//                    Toast.makeText(this, "ERROR EN REGISTRO", Toast.LENGTH_LONG).show();
+//                }
+                break;
+            case R.id.btnCancel:
+                break;
+        }
+
+        finish(); // para que cierre la activity Register.
     }
 }
